c8e91eadd728502c172819811c109155c803cc9e
humayun-ahmad <humayunraj789@gmail.com>
2022-11-23 23:34:10 +0600

    Command pattern

---
+++ Command Pattern/Commands.java
@@ -0,0 +1,18 @@
+public class Commands{
+    public void walkForward(){
+        System.out.println("Walking forward..");
+    }
+    public void stop(){
+        System.out.println("Stopped");
+    }
+    public void turnLeft(){
+        System.out.println("Turned left");
+    }
+    public void turnRight(){
+        System.out.println("Turned right");
+    }
+    public void goBackward(){
+        System.out.println("Walking backward..");
+    }
+
+}
